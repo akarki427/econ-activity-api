55ccda1000e27f5c67fb712294ff35490c59d313
akarki427 <abishekkarki427@gmail.com>
2025-03-08 14:15:10 -0500

    init commit

---
+++ src/test/java/com/dxc/api/econ_activity_api/EconActivityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxc.api.econ_activity_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconActivityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
